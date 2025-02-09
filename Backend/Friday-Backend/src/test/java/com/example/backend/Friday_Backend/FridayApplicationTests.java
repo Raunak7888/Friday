b7aa74b5276632219fe2a44dff16af0549fdec17
Raunak <raunakyadav7888@gmail.com>
2025-02-09 18:20:42 +0530

    Initial commit

---
+++ Backend/Friday-Backend/src/test/java/com/example/backend/Friday_Backend/FridayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend.Friday_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
